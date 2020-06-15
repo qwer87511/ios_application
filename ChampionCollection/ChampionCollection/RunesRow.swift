6d1b5128d1fc2cf1406c5a0898712c25f8b4695c
Chien <a0186163@gmail.com>
2020-06-15 18:21:36 +0800

    .

--- ChampionCollection/ChampionCollection/RunesRow.swift
+++ ChampionCollection/ChampionCollection/RunesRow.swift
@@ -10,11 +10,34 @@ import SwiftUI
 
 struct RunesRow: View {
     let runes:RuneSet
+    @State var picture: Image = Image(systemName: "person")
     @State var count = 0
+    
+    func loadImage() {
+        URLSession.shared.dataTask(with: URL(string: "https://ddragon.leagueoflegends.com/cdn/img/\(runes.icon)")!) {
+            (data, response , error) in
+            if let data = data, let image = UIImage(data: data) {
+                self.picture = Image(uiImage: image)
+            }
+            else {
+                print("load fail")
+            }
+        }.resume()
+    }
     
     var body: some View {
         VStack(alignment: .leading){
-            Text(runes.name).font(.title)
+            HStack{
+                picture
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width:40, height:40)
+                //.frame(alignment: .top)
+                //.clipped()
+                .padding()
+                Text(runes.name).font(.title)
+            }
             
                 ForEach(0..<count, id: \.self){index in
                     HStack{
@@ -27,6 +50,7 @@ struct RunesRow: View {
             
         }.onAppear(){
             self.count = self.runes.slots.count
+            self.loadImage()
         }
     }
 }
